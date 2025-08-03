a46a14afae1ec7fc497891981c62da3c4548a292
Óscar Rodríguez Garrucho <oscar.garrucho@gmail.com>
2025-08-03 08:48:09 +0200

    Initial Commit

---
+++ MarvelComicsApp/MarvelComicsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsAppApp.swift
+//  MarvelComicsApp
+//
+//  Created by Oscar R. Garrucho on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
